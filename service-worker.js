cd475f8d2c42116bd40748dbe9ce8666ab3aef84
J0bless <33474369+J0bless@users.noreply.github.com>
2026-08-11 15:59:14 -0400

    Add colorful festival intro scenery (#20)

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'ridehero-shell-v9';
+const CACHE_NAME = 'ridehero-shell-v10';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
   './css/multi-resort.css?v=1',
-  './css/onboarding.css?v=3',
+  './css/onboarding.css?v=4',
   './data/park-catalog.js?v=1',
   './data/ride-aliases.js?v=1',
   './data/walking-graphs.js?v=1',
